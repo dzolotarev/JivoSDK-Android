fd05978f2195baf98e44c0a7691dae99915516f7
Denis Zolotarev <judaeus@gmail.com>
2022-02-18 13:00:10 +0200

    Add sample app to project

---
+++ sample/src/main/java/com/jivosite/jivosdk_android_sample/PushService.kt
@@ -0,0 +1,10 @@
+package com.jivosite.jivosdk_android_sample
+
+import com.jivosite.sdk.push.JivoFirebaseMessagingService
+
+/**
+ * Created on 4/20/21.
+ *
+ * @author Alexander Tavtorkin (av.tavtorkin@gmail.com)
+ */
+class PushService : JivoFirebaseMessagingService()
